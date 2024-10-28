6567efe0859d23fcb1d40453dc838e259159e286
алина <116123766+alinaagnistova@users.noreply.github.com>
2024-10-29 01:16:01 +0300

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,4 +1,4 @@
-create or replace procedure all_tables(current_user text, target_uer text)
+create or replace procedure all_tables(curr_user text, target_uer text)
         language plpgsql
 as
     $$
@@ -8,7 +8,7 @@ as
     has_tables BOOLEAN := FALSE;
     target_user_exists BOOLEAN;
     BEGIN
-    RAISE NOTICE 'Текущий пользователь: %', current_user;
+    RAISE NOTICE 'Текущий пользователь: %', curr_user;
     RAISE NOTICE 'Кому выдаём права доступа: %', target_user;
     SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = target_user) INTO target_user_exists;
     IF NOT target_user_exists THEN
@@ -19,7 +19,7 @@ as
     RAISE NOTICE '--- -------------------------';
     FOR table_name IN
         FROM pg_tables
-        WHERE schemaname = current_user
+        WHERE schemaname = curr_user
             AND(
         -- Проверка является ли текущий пользователь владельцем таблицы
                 EXISTS(
@@ -27,12 +27,12 @@ as
                     FROM pg_class c
                     JOIN pg_roles r ON c.relowner = r.oid
                     WHERE c.relname = pg_tables.tablename
-                    AND r.rolname = current_user
+                    AND r.rolname = curr_user
             )
                 OR EXISTS (
                     SELECT 1
                     FROM information_schema.role_table_grants g
-                    WHERE g.grantee = current_user
+                    WHERE g.grantee = curr_user
                         AND g.privilege_type = 'GRANT OPTION'
                         AND g.table_name = pg_tables.tablename
                         AND g.table_schema = pg_tables.schemaname
